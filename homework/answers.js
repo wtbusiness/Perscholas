cd346ccbe8b361e78879080dc864b1a74ccd4ad5
Wayne Tucker <95285849+waynetuckermedia@users.noreply.github.com>
2022-12-12 10:00:10 -0500

    Wild Wild Life answered

--- homework/answers.js
+++ homework/answers.js
@@ -21,10 +21,33 @@ for (let i = 0; i <= 200; i ++) {
 // Fizz Buzz
 ////////////////////////////////
 
+for (let i = 1; i <= 100; i++) {
+  console.log(i);
+  if (i % 5 === 0) {
+    console.log(i);
+  }else if(i % 3){
+    console.log(i);
+  }
+} 
+
+
 ////////////////////////////////
 // Wild Wild Life
 ////////////////////////////////
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"];
+const sharky = ["Sharky", "shark", 20, "Left Coast"];
+const plantee = ["Plantee", "plant", 5000, "Mordor"];
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"];
+const dart = ["D'Art", "Demogorgan Dog", 2, "Upside Down"];
 
+plantee[2] = 5001
+console.log(plantee);
+wolfy[3] = "Gotham City";
+console.log(wolfy[3]);
+dart.push("Hawkins");
+console.log(dart);
+wolfy[0] = 'Gameboy';
+console.log(wolfy);
 ////////////////////////////////
 // Yell at the Ninja Turtles
 ////////////////////////////////
