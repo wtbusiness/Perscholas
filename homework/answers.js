0b67a5beb4a1db45ed913d0746af0520918b9f82
Wayne Tucker <95285849+waynetuckermedia@users.noreply.github.com>
2022-12-12 14:14:55 -0500

    Where is Waldo answered

--- homework/answers.js
+++ homework/answers.js
@@ -51,19 +51,117 @@ console.log(wolfy);
 ////////////////////////////////
 // Yell at the Ninja Turtles
 ////////////////////////////////
+const ninjaTurtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"];
+
+for (const element of ninjaTurtles) {
+    console.log(element.toUpperCase());
+    element.toUpperCase();
+    
+}
+console.log(ninjaTurtles);
+
+
 
 ////////////////////////////////
 // Methods, Revisited
 ////////////////////////////////
 
+const favMovies = [
+  "Jaws",
+  "The Fellowship of the Ring",
+  "Howl's Moving Castle",
+  "Django Unchained",
+  "Cloud Atlas",
+  "The Usual Suspects",
+  "Toy Story",
+  "Conan the Barbarian",
+  "Titanic",
+  "Harry Potter",
+  "Fried Green Tomatoes",
+  "Volver",
+  "Oculus",
+  "Seven",
+  "Black Panther",
+  "Harry Potter",
+  "Imitation of Life",
+  "Snatch",
+  "Fast and Furious",
+];
+
+let slicedElement = favMovies.slice(9);
+
+
+console.log(favMovies[8]);
+favMovies.sort();
+console.log(favMovies);
+favMovies.splice(3, 0,'Avatar');
+console.log(favMovies);
+
+favMovies.pop();
+console.log(favMovies);
+favMovies.push("Guardians of the Galaxy");
+console.log(favMovies);
+favMovies.reverse();
+console.log(favMovies);
+favMovies.shift();
+console.log(favMovies);
+
+console.log(favMovies.slice(9));
+console.log(favMovies.length);
+console.log(favMovies.slice(9));
+console.log(slicedElement);
+
+
+
+
+// sorted movies front and back
 ////////////////////////////////
 // Where is Waldo
 ////////////////////////////////
+const whereIsWaldo = [
+  ["Timmy", "Frank"],
+  "Eggbert",
+  ["Lucinda", "Jacc", "Neff", "Snoop"],
+  ["Petunia", ["Baked Goods", "Waldo"]],
+];
+
+console.log("before ",whereIsWaldo);
+for (let i = 0; i < whereIsWaldo.length; i++){
+    if (whereIsWaldo[i].includes("Neff")) {
+        whereIsWaldo[i].splice(2, 1, "No one")
+    }
+}
+console.log("after ", whereIsWaldo);
+console.log(whereIsWaldo);
+console.log(whereIsWaldo);
+whereIsWaldo.splice(1, 1);
+console.log(whereIsWaldo);
+
+
+console.log(whereIsWaldo);
 
 ////////////////////////////////
 //  Excited Kitten
 ////////////////////////////////
 
+// const kittyTalk = [
+//   "Love me, pet me, HSSS!",
+//   "...human...why you taking pictures of me?...",
+//   "...the catnip made me do it...",
+//   "...why does the red dot always get away..."];
+let kittyTalk = [];
+let meow = 0;
+let HSSS = 1;
+
+
+for (let i = 0; i <= 20; i++) {
+  console.log(`Love me, pet me, HSSS! `);
+  if (i % 2 === 0) {
+    console.log(kittyTalk[meow]);
+    console.log(kittyTalk);
+  }
+} 
+console.log(kittyTalk);
 ////////////////////////////////
 //  Find the Median
 ////////////////////////////////
